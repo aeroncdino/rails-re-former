58b287952e91a3b45a15b4248c8c9707054f6102
aeroncdino <dinoaeron@gmail.com>
2023-08-28 11:16:26 +0800

    Final commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
+
     def new
         @user = User.new
       end
     
-      def create
+    def create
         @user = User.new(user_params)
         if @user.save
             redirect_to new_user_path
@@ -12,9 +14,41 @@ class UsersController < ApplicationController
           end
       end
     
-      private
+      
+
+    # def create
+    #   @user = User.new
     
+    #   @user.username = params[:user][:username]
+    #   @user.email = params[:user][:email]
+    #   @user.password = params[:user][:password]
+    
+    #   if @user.save
+    #     redirect_to new_user_path
+    #   else
+    #     render 'new'
+    #   end
+    # end
+
+    def edit
+      render 'edit'
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to user_path(@user)
+      else
+        render 'edit'
+      end
+    end
+
+    private
+
+      def find_user
+        @user = User.find(params[:id])
+      end
       def user_params
         params.require(:user).permit(:username, :email, :password)
       end
+    
 end
